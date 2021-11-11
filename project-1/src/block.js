9b87daf5a45bef6d104d67d417a4ab66a5f625ed
Sergio L. Benítez D <sergio.kun21@gmail.com>
2021-11-11 08:40:24 -0500

    project-1: constructing a block

--- project-1/src/block.js
+++ project-1/src/block.js
@@ -15,18 +15,18 @@ const hex2ascii = require('hex2ascii');
 class Block {
 
     // Constructor - argument data will be the object containing the transaction data
-	constructor(data){
-		this.hash = null;                                           // Hash of the block
-		this.height = 0;                                            // Block Height (consecutive number of each block)
-		this.body = Buffer.from(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
-		this.time = 0;                                              // Timestamp for the Block creation
-		this.previousBlockHash = null;                              // Reference to the previous Block Hash
+    constructor(data) {
+        this.hash = null;                                           // Hash of the block
+        this.height = 0;                                            // Block Height (consecutive number of each block)
+        this.body = Buffer.from(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
+        this.time = 0;                                              // Timestamp for the Block creation
+        this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
-    
+
     /**
      *  validate() method will validate if the block has been tampered or not.
      *  Been tampered means that someone from outside the application tried to change
-     *  values in the block data as a consecuence the hash of the block should be different.
+     *  values in the block data as a consequence the hash of the block should be different.
      *  Steps:
      *  1. Return a new promise to allow the method be called asynchronous.
      *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
@@ -37,15 +37,22 @@ class Block {
      */
     validate() {
         let self = this;
+
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
-                                            
+            let auxHash = self.hash;
             // Recalculate the hash of the Block
+            self.hash = SHA256(JSON.stringify({ ...self, hash: null })).toString();
             // Comparing if the hashes changed
-            // Returning the Block is not valid
-            
-            // Returning the Block is valid
-
+            if (auxHash === self.hash) {
+                // Returning the Block is valid
+                resolve(true);
+            }
+            else {
+                // Returning the Block is not valid
+                resolve(false);
+                reject(Error('Block is not valid'));
+            }
         });
     }
 
@@ -59,14 +66,23 @@ class Block {
      *     or Reject with an error.
      */
     getBData() {
-        // Getting the encoded data saved in the Block
-        // Decoding the data to retrieve the JSON representation of the object
-        // Parse the data to an object to be retrieve.
+        let self = this;
 
-        // Resolve with the data if the object isn't the Genesis block
-
+        return new Promise((resolve, reject) => {
+            // Getting the encoded data saved in the Block
+            const encodedDataString = self.body;
+            // Decoding the data to retrieve the JSON representation of the object
+            const decodedDataString = hex2ascii(encodedDataString);
+            // Parse the data to an object to be retrieve.
+            const decodedDataObject = JSON.parse(decodedDataString);
+            // Resolve with the data if the object isn't the Genesis block
+            if (self.height > 0) {
+                resolve(decodedDataObject);
+            } else {
+                reject(new Error('Cannot decode the data for Genesis Block'));
+            }
+        })
     }
-
 }
 
 module.exports.Block = Block;                    // Exposing the Block class as a module
