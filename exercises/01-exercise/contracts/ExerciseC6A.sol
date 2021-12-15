fe971868713abe55bf5f536c2fb0906e059bb0ee
Sergio L. Benítez D <sergio.kun21@gmail.com>
2021-12-14 22:49:06 -0500

    exercises: complete smart contracts operations

--- exercises/01-exercise/contracts/ExerciseC6A.sol
+++ exercises/01-exercise/contracts/ExerciseC6A.sol
@@ -1,11 +1,9 @@
 pragma solidity ^0.4.25;
 
 contract ExerciseC6A {
-
     /********************************************************************************************/
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
-
 
     struct UserProfile {
         bool isRegistered;
@@ -15,7 +13,7 @@ contract ExerciseC6A {
     address private contractOwner;                  // Account used to deploy contract
     mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
 
-
+    bool private operational = true;
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -27,11 +25,7 @@ contract ExerciseC6A {
     * @dev Constructor
     *      The deploying account becomes contractOwner
     */
-    constructor
-                                (
-                                ) 
-                                public 
-    {
+    constructor () public {
         contractOwner = msg.sender;
     }
 
@@ -45,51 +39,75 @@ contract ExerciseC6A {
     /**
     * @dev Modifier that requires the "ContractOwner" account to be the function caller
     */
-    modifier requireContractOwner()
-    {
+    modifier requireContractOwner() {
         require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    /**
+     * @dev Modifier that requires the "operational" boolean variable to be "true"
+     * This is used on all state changing functions to pause the contract in 
+     * the event there is an issue that needs to be fixed
+     */
+    modifier requireIsOperational() {
+        require(operational, "Contract is currently not operational");
         _;
     }
 
     /********************************************************************************************/
     /*                                       UTILITY FUNCTIONS                                  */
     /********************************************************************************************/
-
-   /**
-    * @dev Check if a user is registered
-    *
-    * @return A bool that indicates if the user is registered
-    */   
-    function isUserRegistered
-                            (
-                                address account
-                            )
-                            external
-                            view
-                            returns(bool)
+    
+    /**
+     * @dev Check if a user is registered
+     * @return A bool that indicates if the user is registered
+     */
+    function isUserRegistered(address account)
+        external
+        view
+        returns(bool)
     {
         require(account != address(0), "'account' must be a valid address.");
         return userProfiles[account].isRegistered;
+    }
+
+    /**
+    * @dev Get operating status of contract
+    * @return A bool that is the current operating status
+    */      
+    function isOperational()
+        public
+        view
+        return(bool)
+    {
+        return operational
+    }
+    /**
+     * @dev Sets contract operations on/off. When operational mode is disabled,
+     * all write transactions except for this one will fail
+     * @return void
+     */
+    function setOperatingStatus(bool mode)
+        external
+        requireContractOwner
+    {
+        operational = mode;
     }
 
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
 
-    function registerUser
-                                (
-                                    address account,
-                                    bool isAdmin
-                                )
-                                external
-                                requireContractOwner
+    function registerUser(address account, bool isAdmin)
+        external
+        requireIsOperational
+        requireContractOwner
     {
         require(!userProfiles[account].isRegistered, "User is already registered.");
 
         userProfiles[account] = UserProfile({
-                                                isRegistered: true,
-                                                isAdmin: isAdmin
-                                            });
+            isRegistered: true,
+            isAdmin: isAdmin
+        });
     }
 }
-
