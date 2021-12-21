3b1c549c27d9533b7badee3241718e911d231672
Sergio L. Benítez D <sergio.kun21@gmail.com>
2021-12-21 13:06:35 -0500

    project-03: add functions signatures

--- projects/03-project-dapp-for-tracking-items/contracts/SupplyChain.sol
+++ projects/03-project-dapp-for-tracking-items/contracts/SupplyChain.sol
@@ -10,7 +10,7 @@ contract SupplyChain {
     // Variables
     //----------------------------------
 
-    uint256 sku;
+    uint256 skuCounter;
     mapping(uint256 => ChontaduroItem) chontaduroItems;
     mapping(uint256 => JuiceItem) juiceItems;
     mapping(uint256 => uint256[]) juiceChontaduros;
@@ -28,7 +28,7 @@ contract SupplyChain {
         Certified,
         Packed,
         ReadyForSale,
-        Purchased
+        Sold
     }
     JuiceState constant defaultJuiceState = JuiceState.Created;
     struct ChontaduroItem {
@@ -61,4 +61,127 @@ contract SupplyChain {
     //----------------------------------
     // Events
     //----------------------------------
+
+    event ChontaduroPlanted(uint256 chontaduroUpc);
+    event ChontaduroHarvested(uint256 chontaduroUpc);
+    event ChontaduroAudited(uint256 chontaduroUpc);
+    event ChontaduroProcessed(uint256 chontaduroUpc);
+
+    event JuiceCreated(uint256 juiceUpc);
+    event JuiceBlended(uint256 juiceUpc);
+    event JuiceProduced(uint256 juiceUpc);
+    event JuiceCertified(uint256 juiceUpc);
+    event JuicePacked(uint256 juiceUpc);
+    event JuiceReadyForSale(uint256 juiceUpc);
+    event JuiceSold(uint256 juiceUpc);
+
+    //----------------------------------
+    // Modifiers
+    //----------------------------------
+
+    modifier verifyCaller(address _address) {
+        _;
+    }
+
+    modifier paidEnough(uint256 _price) {
+        _;
+    }
+
+    modifier checkValue(uint256 _juiceUpc) {
+        _;
+    }
+
+    // Chontaduro Modifiers
+    //----------------------------------
+
+    modifier isPlanted(uint256 _chontaduroUpc) {
+        _;
+    }
+
+    modifier isHarvested(uint256 _chontaduroUpc) {
+        _;
+    }
+
+    modifier isAudited(uint256 _chontaduroUpc) {
+        _;
+    }
+
+    modifier isProcessed(uint256 _chontaduroUpc) {
+        _;
+    }
+
+    // Juice Modifiers
+    //----------------------------------
+
+    modifier isCreated(uint256 _juiceUpc) {
+        _;
+    }
+
+    modifier isBlended(uint256 _juiceUpc) {
+        _;
+    }
+
+    modifier isProduced(uint256 _juiceUpc) {
+        _;
+    }
+
+    modifier isPacked(uint256 _juiceUpc) {
+        _;
+    }
+
+    modifier isCertified(uint256 _juiceUpc) {
+        _;
+    }
+
+    modifier isReadyForSale(uint256 _juiceUpc) {
+        _;
+    }
+
+    modifier isSold(uint256 _juiceUpc) {
+        _;
+    }
+
+    //----------------------------------
+    // Constructor
+    //----------------------------------
+
+    constructor() public payable {
+        skuCounter = 1;
+    }
+
+    //----------------------------------
+    // Functions
+    //----------------------------------
+
+    function kill() public onlyOwner {}
+
+    // Chontaduro Functions
+    //----------------------------------
+
+    function chontaduroPlantItem() public {}
+
+    function chontaduroHarvestItem() public {}
+
+    function chontaduroAuditItem() public {}
+
+    function chontaduroProcessItem() public {}
+
+    function fetchChontaduroItem() public {}
+
+    // Juice Functions
+    //----------------------------------
+
+    function juiceCeratetItem() public {}
+
+    function juiceBlendItem() public {}
+
+    function juiceProduceItem() public {}
+
+    function juiceCertifyItem() public {}
+
+    function juiceSellItem() public {}
+
+    function juiceBuyItem() public {}
+
+    function fetchJuiceItem() public {}
 }
