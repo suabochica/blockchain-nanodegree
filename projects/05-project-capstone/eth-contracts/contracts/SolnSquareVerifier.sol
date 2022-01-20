7a3e8afb0aed07549dfe7388e6d0158ae079b51e
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-01-20 07:40:27 -0500

    project-05: implement solution square verifier

--- projects/05-project-capstone/eth-contracts/contracts/SolnSquareVerifier.sol
+++ projects/05-project-capstone/eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,118 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import "./ERC721MintableComplete.sol";
 
+// ✅ define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is ERC721MintableComplete {
+    Verifier private verifierContract;
 
+    // ✅ define a solutions struct that can hold an index & an address
+    struct Solution {
+        bool minted;
+        uint256 solutionIndex;
+        address solutionAddress;
+    }
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+    // ✅ define an array of the above struct
+    uint256 numberOfSolutions;
+    Solution[] private solutions;
 
+    // ✅ define a mapping to store unique solutions submitted
+    mapping(bytes => Solution) solutions;
 
+    // ✅ Create an event to emit when a solution is added
+    event SolutionAdded(uint256 solutionIndex, address indexed solutionAddress);
 
-// TODO define a solutions struct that can hold an index & an address
+    constructor(
+        address verifierAddress,
+        string memory name,
+        string memory symbol
+    ) public ERC721MintableComplete(name, symbol) {
+        verifierContract = Verifier(verifierAddress);
+    }
 
+    // ✅ Create a function to add the solutions to the array and emit the event
+    function addSolution(
+        uint256[2] memory A,
+        uint256[2] memory A_p,
+        uint256[2] memory B,
+        uint256[2] memory B_p,
+        uint256[2] memory C,
+        uint256[2] memory C_p,
+        uint256[2] memory H,
+        uint256[2] memory K,
+        uint256[2] memory input
+    ) public {
+        bytes32 solutionHash = keccak256(abi.encodePacked(input[0], input[1]));
+        require(
+            solutions[solutionHash].solutionAddress == address(0),
+            "Solution address already exists"
+        );
 
-// TODO define an array of the above struct
+        bool verified = verifierContract.verifyTransaction(
+            A,
+            A_p,
+            B,
+            B_p,
+            C,
+            C_p,
+            H,
+            K,
+            input
+        );
+        require(verified, "Solution could not be verified");
 
+        solutions[solutionHash] = Solution(
+            numberOfSolutions,
+            msg.sender,
+            false
+        );
+        solutions.push(solution);
 
-// TODO define a mapping to store unique solutions submitted
+        emit SolutionAdded(numberOfSolutions, msg.sender);
+        numberOfSolutions++;
+    }
 
+    // ✅ Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mintNewNFT(
+        uint256 a,
+        uint256 b,
+        address to
+    ) public {
+        bytes32 solutionHash = keccak256(abi.encodePacked(a, b));
 
+        require(
+            soutions[solutionHash].solutionAddress != address(0),
+            "Solution address already exists"
+        );
+        require(
+            soutions[solutionHash].solutionAddress != msg.sender,
+            "Only solution address can be use to mint a token"
+        );
+        require(
+            soutions[solutionHash].minted != false,
+            "Token is already minted"
+        );
 
-// TODO Create an event to emit when a solution is added
+        super.mint(to, solutions[solutionHash].solutionIndex);
 
+        solutions[solutionHash].minted = true;
+    }
+}
 
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ✅ define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+contract Verifier {
+    function verifyTransaction(
+        uint256[2] memory A,
+        uint256[2] memory A_p,
+        uint256[2] memory B,
+        uint256[2] memory B_p,
+        uint256[2] memory C,
+        uint256[2] memory C_p,
+        uint256[2] memory H,
+        uint256[2] memory K,
+        uint256[2] memory input
+    ) public returns (bool response);
+}
